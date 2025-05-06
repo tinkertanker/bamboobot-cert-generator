85d7ec5d91c978066ca472cc253190d2976412e5
YJ Soon <yjsoon@tinkertanker.com>
2025-05-06 12:05:19 +0800

    Add Jest testing infrastructure and dependencies
    - Add Jest and React Testing Library dependencies
    - Configure Jest with Next.js integration
    - Add testing setup file with mocks for Next.js components

---
+++ jest.setup.js
@@ -0,0 +1,32 @@
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+
+// Mock next/image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return <img {...props} />;
+  },
+}));
+
+// Mock next/navigation hooks
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+    back: jest.fn(),
+    forward: jest.fn(),
+    refresh: jest.fn(),
+    pathname: '/',
+    query: {},
+  })),
+  usePathname: jest.fn(() => '/'),
+  useSearchParams: jest.fn(() => ({
+    get: jest.fn(),
+  })),
+}));
+
+// Mock the global fetch
+global.fetch = jest.fn();
